7d607304eb4f49cbb91a316f026e91778fc1cc8c
fernandojm94 <69207654+fernandojm94@users.noreply.github.com>
2021-04-21 10:17:18 -0500

    Fix acciones de la aprobación

--- model/empresa/aprobar.php
+++ model/empresa/aprobar.php
@@ -1,36 +1,47 @@
 <?php
+	
 	include('../../controller/empresas/funciones_empresa.php');
+	
+	echo '<script type="text/javascript" src="../../assets/jquery/jquery-3.4.1.js"></script>
+		  <script type="text/javascript" src="../../assets/js/sweetalert.min.js"></script>';
 
 	$id = $_GET['id'];
 	$status = $_GET['status'];
 
-	if(update_status($id, $status)){
-		if($status == 1){
-			$mensaje = "Aprobado correctamente";
-		}else{
-			$mensaje = "Rechazado correctamente"; 
-		}?>
 
-		<script type="text/javascript">
-			swal({
-                title: <?php echo $mensaje ?>,
-                icon: "success",
-                button: "Aceptar"
-            }).then(function(){
-            	pagecontent('view/bolsa/listado.php');
-            });
-		</script>
-	<?php}else{?>
-		<script type="text/javascript">
-			swal({
-                title: "Error al guardar!",
-                icon: "error",
-                button: "Aceptar"
-            }).then(function(){
-            	pagecontent('view/bolsa/listado.php');
-            });
-		</script>
-	<?php}
-	
-		$url = "'http://desarrollo-economico.test'";
-	$mensaje = '<meta http-equiv="Refresh" content="3; url='.$url.' "/>';
+	if(update_status($id, $status))
+	{
+		if($status == 1){
+			$mensaje = "'Aprobado correctamente'";
+		}else{
+			$mensaje = "'Rechazado correctamente'"; 
+		}
+
+		echo '<script type="text/javascript">
+				$(document).ready( function () {
+					swal({
+		                title: '.$mensaje.',
+		                icon: "success",
+		                button: "Aceptar"
+		            }).then(function(){
+		            	window.location.href = "http://desarrollo-economico.test";
+		            });
+				});
+			 </script>';
+
+	}
+	else
+	{
+		echo
+			'<script type="text/javascript">
+			$(document).ready( function () {
+				swal({
+	                title: "Error al guardar!",
+	                icon: "error",
+	                button: "Aceptar"
+	            }).then(function(){
+	            	window.location.href = "http://desarrollo-economico.test";
+	            });
+	        });    
+			</script>';
+	}
